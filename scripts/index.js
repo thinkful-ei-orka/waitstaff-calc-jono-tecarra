8e7170e4995d85ba1e71de43dd884b2b1445a05e
Jon <jdog992@gmail.com>
2020-04-28 10:54:16 -0400

    started greybox wireframe

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+/* take in meal price, tax rate, and tip percentage for a meal
+returns the meal subtotal + tip to give a total
+stores all tips an returns the total 
+tracks how many meals have been paid for
+calculate the average tip per meal*/
